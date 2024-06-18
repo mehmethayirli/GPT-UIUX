3efc3bf24c23b11f3c3fe8464f3b0d6b89cd7692
Mehmet <mehmethayirli0@gmail.com>
2024-06-18 16:53:08 +0300

    navbar menu

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,6 +2,6 @@ export { default as Blog } from "./blog/Blog";
 export { default as Features } from "./features/Features";
 export { default as Footer } from "./footer/Footer";
 export { default as Header } from "./header/Header";
-export { default as Navbar } from "./navbar/Navbar";
+export { default as Navbar } from "../components/navbar/Navbar";
 export { default as Possibility } from "./possibility/Possibility";
 export { default as WhatGPT } from "./whatGPT/WhatGPT";
