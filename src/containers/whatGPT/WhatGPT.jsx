4225aa139b2d3e28ec7ba2803d324277b55f827a
Mehmet <mehmethayirli0@gmail.com>
2024-06-28 11:40:00 +0300

    wgpt classname fix

--- src/containers/whatGPT/WhatGPT.jsx
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -5,7 +5,7 @@ import Feature from "../../components/feature/Feature";
 const WhatGPT = () => {
   return (
     <div className="gpt__whatgpt section__margin" id="wgpt">
-      <div className="gpt__whatgpt-features">
+      <div className="gpt__whatgpt-feature">
         <Feature
           title="What is GPT"
           text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
