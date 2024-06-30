2e48b02a4a78717856a14d63ec31048204e9ba22
Mehmet <mehmethayirli0@gmail.com>
2024-06-30 17:22:53 +0300

    cta.jsx

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="gpt__cta">
+      <div className="gpt__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
