f2a77c1fdd30f17d585c3dc34472071e630fac10
Mehmet <mehmethayirli0@gmail.com>
2024-07-01 16:58:45 +0300

    article.jsx finish

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./article.css";
 
-const Article = ({ imgUrl }) => {
+const Article = ({ imgUrl, date, text }) => {
   return (
     <div className="gpt__blog-container_article">
-      <div className="gpt__blog-container_article-image">
-        <img src={imgUrl} alt="blog image" />
+      <div className="gpt__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
       </div>
     </div>
   );
