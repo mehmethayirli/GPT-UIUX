b2064845b7a922f7a30ab8313cf82098a6200797
Mehmet <mehmethayirli0@gmail.com>
2024-07-01 15:47:29 +0300

    article.jsx

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl }) => {
+  return (
+    <div className="gpt__blog-container_article">
+      <div className="gpt__blog-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+    </div>
+  );
 };
 
 export default Article;
