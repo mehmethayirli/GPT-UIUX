0b0cd480ece00e3099a01f26f2cb217e82e93045
Mehmet <mehmethayirli0@gmail.com>
2024-07-03 11:38:21 +0300

    feature div update

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -4,12 +4,13 @@ import "./feature.css";
 const Feature = ({ title, text }) => (
   <div className="gpt__features-container__feature">
     <div className="gpt__features-container__feature-title">
-      <div />
       <h1>{title}</h1>
+      <div />
     </div>
     <div className="gpt__features-container_feature-text">
       <p>{text}</p>
     </div>
   </div>
 );
+
 export default Feature;
